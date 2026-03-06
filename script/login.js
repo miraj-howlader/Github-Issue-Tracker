5da09949ff89eaf15c6046a14ef620bec356402d
miraj <mirajhowlader9999@gmail.com>
2026-03-06 23:49:28 +0600

    'login'

---
+++ script/login.js
@@ -0,0 +1,14 @@
+function login() {
+
+  const username = document.getElementById("username").value
+  const password = document.getElementById("password").value
+
+  if(username === "admin" && password === "admin123"){
+
+    localStorage.setItem("auth", "true")
+    window.location.href = "dashboard.html"
+
+  } else {
+    alert("Invalid Credentials")
+  }
+}
